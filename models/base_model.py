c7122b953e348753230992bb5d086cb8ed0ef6a0
Thibaut <thibautbernard@sfr.fr>
2021-02-10 16:26:27 +0000

    feat(): serialize and deserialize

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from datetime import datetime
-from models.__init__ import storage
-#import models
+#from models.__init__ import storage
+import models
 import uuid
 #from engine.file_storage import FileStorage
 """
@@ -18,7 +18,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             reserved_attribute = ['created_at', 'updated_at']
             for i in kwargs:
@@ -37,7 +37,7 @@ class BaseModel:
     def save(self):
         """ Update attribute updated_at"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ Return a dict of object attributes and methods"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,25 +3,29 @@ import json
 import os.path
 import sys
 from os import path
-import models
-#from models.base_model import BaseModel
+from models.base_model import BaseModel
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         return FileStorage.__objects
+
     def new(self, obj):
         name_class = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects.update({name_class : obj.to_dict()}) #Objct or dict directly
+        FileStorage.__objects.update({name_class : obj})
+
     def save(self):
+        for i in FileStorage.__objects:
+            FileStorage.__objects[i] = FileStorage.__objects[i].to_dict()
         with open(self.__file_path, 'w') as f:
-                f.write(json.dumps(FileStorage.__objects))
+            f.write(json.dumps(FileStorage.__objects))
+
     def reload(self):
         if path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path) as f:
                 json_f = f.read()
                 deserial = json.loads(json_f)
-            #for i in deserial:
-            #    FileStorage.__objects[i] = BaseModel(deserial[i])
-            FileStorage.__objects = deserial
+            for i in deserial:
+                FileStorage.__objects[i] = BaseModel(**deserial[i])
+            #FileStorage.__objects = deserial
